88ef2612e034a3b7105089cd81c2cb784f7725d2
ander428 <ander428@mail.chapman.edu>
2020-07-06 17:37:22 -0700

    change from md to html

--- src/update_md.sh
+++ src/update_md.sh
@@ -2,23 +2,31 @@
 
 for i in *.ipynb
 do
-    jupyter.exe nbconvert --to markdown --no-input "$i"
+    jupyter.exe nbconvert --to html --template full --no-input "$i"
 
-    if [[ "${i%%.*}" == *"_"* ]]; then
-      A="$(cut -d'_' -f1 <<<"${i%%.*}")"
-      B="$(cut -d'_' -f2 <<<"${i%%.*}")"
-
-      echo -e "---\ntitle: ${A^} ${B^} \nlayout: default\nfilename: ${i%%.*}\n---" | cat - "${i%%.*}.md" | sponge "${i%%.*}.md"
-    else
-       echo -e "---\ntitle: ${i%%.*} \nlayout: default\nfilename: ${i%%.*}\n---" | cat - "${i%%.*}.md" | sponge "${i%%.*}.md"
+    
+    if ! grep -q "${i%%.*}" "../_config.yml"; then
+      if [[ "${i%%.*}" == *"_"* ]]; then
+        A="$(cut -d'_' -f1 <<<"${i%%.*}")"
+        B="$(cut -d'_' -f2 <<<"${i%%.*}")"
+        title="${A^} ${B^}"
+        # echo -e "---\ntitle: ${A^} ${B^} \nlayout: default\nfilename: ${i%%.*}\n---" | cat - "${i%%.*}.md" | sponge "${i%%.*}.md"
+      else
+        title="${i%%.*}"
+        # echo -e "---\ntitle: ${i%%.*} \nlayout: default\nfilename: ${i%%.*}\n---" | cat - "${i%%.*}.md" | sponge "${i%%.*}.md"
+      fi
+        echo ""  | tee -a "../_config.yml"
+        echo "  - title: ${title}"  | tee -a "../_config.yml"
+        echo "    url: ${i%%.*}.html"  | tee -a "../_config.yml"
     fi
 done
 
-mv *.md ../
+mv *.html ../
 
 if [[ `git status --porcelain` ]]; then
-    git add ./*.ipynb ../*.md
-    git commit -m "Code and Markdown Autoupdate `date`"
+    git add ./*.ipynb ../*.html
+    git commit -m "Code and HTML Autoupdate `date`"
     git push
     echo "Changes Uploaded!"
 fi
+
