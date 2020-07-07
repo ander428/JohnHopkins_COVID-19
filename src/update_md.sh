2c6c3758496be1a795126ba0681ac4ac2347037c
ander428 <ander428@mail.chapman.edu>
2020-07-06 19:17:41 -0700

    updating template

--- src/update_md.sh
+++ src/update_md.sh
@@ -4,24 +4,23 @@ for i in *.ipynb
 do
     jupyter.exe nbconvert --to html --template full --no-input "$i"
 
-    
+    if [[ "${i%%.*}" == *"_"* ]]; then
+      A="$(cut -d'_' -f1 <<<"${i%%.*}")"
+      B="$(cut -d'_' -f2 <<<"${i%%.*}")"
+      title="${A^} ${B^}"
+      echo -e "---\ntitle: ${A^} ${B^} \nlayout: default\nfilename: ${i%%.*}\n---\n" | cat - "${i%%.*}.html" | sponge "${i%%.*}.html"
+    else
+      title="${i%%.*}"
+      echo -e "---\ntitle: ${i%%.*} \nlayout: default\nfilename: ${i%%.*}\n---\n" | cat - "${i%%.*}.html" | sponge "${i%%.*}.html"
+    fi
     if ! grep -q "${i%%.*}" "../_config.yml"; then
-      if [[ "${i%%.*}" == *"_"* ]]; then
-        A="$(cut -d'_' -f1 <<<"${i%%.*}")"
-        B="$(cut -d'_' -f2 <<<"${i%%.*}")"
-        title="${A^} ${B^}"
-        # echo -e "---\ntitle: ${A^} ${B^} \nlayout: default\nfilename: ${i%%.*}\n---" | cat - "${i%%.*}.md" | sponge "${i%%.*}.md"
-      else
-        title="${i%%.*}"
-        # echo -e "---\ntitle: ${i%%.*} \nlayout: default\nfilename: ${i%%.*}\n---" | cat - "${i%%.*}.md" | sponge "${i%%.*}.md"
-      fi
-        echo ""  | tee -a "../_config.yml"
-        echo "  - title: ${title}"  | tee -a "../_config.yml"
-        echo "    url: ${i%%.*}.html"  | tee -a "../_config.yml"
+      echo ""  | tee -a "../_config.yml"
+      echo "  - title: ${title}"  | tee -a "../_config.yml"
+      echo "    url: ${i%%.*}.html"  | tee -a "../_config.yml"
     fi
 done
 
-mv *.html ../_pages/
+mv *.html ../
 
 if [[ `git status --porcelain` ]]; then
     git add ./*.ipynb ../*.html ../_config.yml
